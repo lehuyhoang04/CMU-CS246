d83bba15d922a1901dae090b5dc295598ea65809
lehuyhoang04 <hoangdepzai462004@gmail.com>
2025-03-17 19:24:02 +0700

    them class User

---
+++ UserApp/src/main/java/com/mycompany/userapp/User.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.userapp;
+
+/**
+ *
+ * @author MSI
+ */
+public class User { 
+private String userId; 
+private String email; 
+public User(String userId, String email) { 
+this.userId = userId; 
+this.email = email; 
+} 
+public String getEmail() { return email; } 
+public void setEmail(String email) { this.email = email; } 
+public void updateEmail(String newEmail) { this.email = email; } 
+}
